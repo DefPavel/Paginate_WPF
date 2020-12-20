be09bf8a2bb0b4083216e6662610611307a16066
DefPavel <33838515+DefPavel@users.noreply.github.com>
2020-12-20 11:26:13 +0300

    add project

---
+++ Items.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastedAsyncLoadDataGrid
+{
+    public class Items
+    {
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private int _Id;
+
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+    
+
+        public void GetDataAsync()
+        {
+
+        }
+
+    }
+
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace FastedAsyncLoadDataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly PagingCollectionView _cview;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // 1 Параметр Данные,2 по сколько записей выводить?
+            this._cview = new PagingCollectionView(
+               GetData(),
+               100
+           );
+            this.DataContext = this._cview;
+
+        }
+
+
+        public List<Items> GetData()
+        {
+            List<Items> list = new List<Items>();
+            for (int i = 0; i<= 15000; i++)
+            {
+                list.Add(new Items { Id = new Random().Next(), Name = "АУЕЕЕ" });
+                
+            }
+            return list;
+        }
+
+        private void OnNextClicked(object sender, RoutedEventArgs e)
+        {
+            this._cview.MoveToNextPage();
+        }
+
+        private void OnPreviousClicked(object sender, RoutedEventArgs e)
+        {
+            this._cview.MoveToPreviousPage();
+        }
+    }
+}
